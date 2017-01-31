ea6ac7fda445d9e9aa5e6051d484a4d0b1268ba1
muirurikin <muirurikin@gmail.com>
2017-01-31 20:37:02 +0300

    Modify app.js and layout files

---
+++ app.js
@@ -0,0 +1,19 @@
+//import module
+var express = require('express'); 
+
+//create express app
+var app = express(); 
+
+app.disable('x-powered-by');
+
+//set port
+app.set('port', process.env.PORT || 3000); 
+
+app.get("/", function(req, res) {
+    res.sendFile("/home/alexona/Documents/Moods-app/views/" + "index.html");
+});
+
+//start server
+app.listen(app.get('port'), function() {
+     console.log('Express app running on http://localhost:' + app.get('port') + ' Press CTRL-C to terminate. ');
+ });
