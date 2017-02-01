7f441aece93b8d56ffca82fcbf96c4a450f9ec5a
Harrisonkamau <kamauharrison87@gmail.com>
2017-02-01 21:26:10 +0300

    register && login users using passport local

--- app.js
+++ app.js
@@ -1,40 +1,89 @@
-// set mongoDB uri
-if(!process.env.MONGODB_URI) {
-  require('dotenv').config();
-}
-
-//import required modules
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-var routes = require('./server/routes');
-
-//create express app
-var app = express();
-
-
-//set port
-var port = process.env.PORT || 3000;
-
-// use global Promise to bypass deprecation errors
+// use global promise
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb://localhost/moodsapp');
 var db = mongoose.connection;
 
-// notify the user when there is a connection
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Successfully connected to db');
+var routes = require('./server/routes/index');
+var users = require('./server/routes/users');
+
+// Init App
+var app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
 });
 
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static('public'));
-routes(app);
+app.use('/', routes);
+app.use('/users', users);
 
+// Set Port
+app.set('port', (process.env.PORT || 3000));
 
-//start server
-app.listen(port, function() {
-     console.log('Express app running on http://localhost:'+ port );
- });
+// start server
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
+});
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,18 @@
-var path = require('path');
+var express = require('express');
+var router = express.Router();
 
-var routes = function(app) {
-    app.route('/')
-    app.get(function (req, res) {
-           res.sendFile('index.html', { root: path.join(__dirname, '../../public')});
-       });
-       
+// Get homepage
+router.get('/', ensureAuthenticated, function(req, res){
+	res.render('index');
+});
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		// req.flash('error_msg','You are not logged in');
+		res.redirect('/users/login');
+	}
 }
-module.exports = routes;
+
+module.exports = router;
