890a23fae73f6841dcef74ddddab85e7c6ffdd10
Harrisonkamau <kamauharrison87@gmail.com>
2017-02-01 20:43:17 +0300

    create new branch - harrison-dev

--- app.js
+++ app.js
@@ -1,45 +1,23 @@
-//import module
+//import required modules
 
 var express = require('express');
-var formidable = require('formidable');
 var bodyParser = require('body-parser');
 var routes = require('./server/routes');
 
 //create express app
 var app = express();
 
-app.disable('x-powered-by');
 
 //set port
-app.set('port', process.env.PORT || 3000);
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('public'));
 routes(app);
 
-app.post("/process", function(req, res) {
-    console.log(req.body);
-    res.redirect(303, '/user-profile.html');
-});
-app.get('/user-profile', function(req, res) {
-     var now = new Date();
-     res.render('user-profile.html',{
-         year: now.getFullYear(),
-         month: now.getMonth() });
- });
- app.post('/user-profile/:year/:month', function(req, res) {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, file) {
-        if(err)
-        return res.redirect(303, '/error');
-        console.log('File Received');
-        console.log(file);
-        res.redirect(303, '/user-profile');
-    });
-});
 
 //start server
-app.listen(app.get('port'), function() {
-     console.log('Express app running on http://localhost:' + app.get('port') + ' Press CTRL-C to terminate. ');
+app.listen(port, function() {
+     console.log('Express app running on http://localhost:'+ port );
  });
