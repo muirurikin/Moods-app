105d285f5569b611f5a04bf313b15f32edacdc77
Harrisonkamau <kamauharrison87@gmail.com>
2017-02-22 22:41:09 +0300

    create a user model

---
+++ server/models/user.js
@@ -0,0 +1,16 @@
+// define the user schema
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    first_name: String,
+    last_name: String,
+    email: String,
+    password: String
+
+});
+// create a User model using the define Schema
+var User = mongoose.model('User', UserSchema);
+
+// export the User
+model.exports = User;
