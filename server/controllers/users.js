3743b15906d94addf19b07de40d9c22d652f4c51
Harrisonkamau <kamauharrison87@gmail.com>
2017-02-22 22:48:03 +0300

    import user && create simple guideline

---
+++ server/controllers/users.js
@@ -0,0 +1,12 @@
+/*This is the most important part of an MVC app -The controller
+** It defines how a user will be authenticated
+** How the route will be protected and so on .
+** I have created an arbitrary schema for you.
+** Try created a login and register user function
+** From there I'll be able to show you how to use Passportjs
+** for authentication.
+*/
+
+// import the user model
+var User = require('../models/user');
+console.log(User);
