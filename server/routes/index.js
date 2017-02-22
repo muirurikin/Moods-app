c434fce6b338792e4e74e464437e5199c1bcc0be
Harrisonkamau <kamauharrison87@gmail.com>
2017-02-22 22:30:47 +0300

    remove redundant code

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,12 @@ var path = require('path');
 
 var routes = function(app) {
     app.route('/')
-    app.get(function (req, res) {
+        .get(function (req, res) {
            res.sendFile('index.html', { root: path.join(__dirname, '../../public')});
        });
     app.route('/chatroom')
-    app.post("/home", function (req, res) {
-           console.log(req.body);
+        .post(function (req, res) {
+        //    console.log(req.body);
            res.sendFile('chatroom.html', { root: path.join(__dirname, '../../public')});
        });
 }
