531a10fb66dfd03dcb584d91abfe7f2dce8b727e
Harrisonkamau <kamauharrison87@gmail.com>
2017-02-24 12:50:05 +0300

    remove user routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,24 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-
+  
+    // GET home page
     router.get('/', function (req, res) {
       res.render('index.html');
          });
-    router.get('/login', function (req, res) {
-          res.render('login.html');
-         });
-    router.get('/register', function (req, res) {
-          res.render('register.html');
-         });
-
-    router.post('/login', function (req, res) {
-        console.log(req.body);
-          res.sendFile('chatroom.html', { root: path.join(__dirname, '../../public')});
-         });
-    router.post('/register', function (req, res) {
-        console.log(req.body);
-          res.sendFile('user-profile.html', { root: path.join(__dirname, '../../public')});
-         });
+   
 
 module.exports = router;
