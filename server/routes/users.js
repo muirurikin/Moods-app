f491394e724f7588e8304f0c8e2c1a5e47e1ee4c
Harrisonkamau <kamauharrison87@gmail.com>
2017-02-25 23:26:15 +0300

    save new user to db

--- server/routes/users.js
+++ server/routes/users.js
@@ -32,6 +32,23 @@ router.post('/register', function(req, res){
     var confirmPassword = req.body.password2;
 
     // create a new user and save to db using model User
-    
-})
+    // create newUser object
+    var newUser = new User({
+        name: name,
+        email: email,
+        password: password
+    });
+
+    // save the newUser to User collection
+    User.createUser(newUser, function(err, user){
+        if(err) throw err;
+        // flash a success message if no error thrown
+        req.flash('success_msg', 'You are now registered. Please login');
+
+        // redirect user to login page
+        res.redirect('/users/login')
+    })
+});
+
+module.exports = router;
 
