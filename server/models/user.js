37a747873d1eddab768ee2e8e7b6c132bff4e074
Harrisonkamau <kamauharrison87@gmail.com>
2017-02-26 01:32:55 +0300

    modify user schema

--- server/models/user.js
+++ server/models/user.js
@@ -6,16 +6,24 @@ var bcrypt = require('bcryptjs');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    name: String,
-    email: String,
-    password: String
+    username: {
+        type: String,
+        index: true
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    }
 
 });
 // create a User model using the define Schema
-var User = mongoose.model('User', UserSchema);
+var User = module.exports = mongoose.model('User', UserSchema);
 
 // use bcrypt to hash a new user's password
 module.exports.createUser = function(newUser, callback){
+    console.log('new user:',newUser);
     bcrypt.genSalt(10, function(err, salt){
         bcrypt.hash(newUser.password, salt, function(err, hash){
             // set the user's password equal to the hashed one
@@ -27,5 +35,18 @@ module.exports.createUser = function(newUser, callback){
     });
 }
 
-// export the User
-module.exports = User;
+module.exports.getUserByUsername = function(usernname, callback){
+	var query = {username: username};
+	User.findOne(query, callback);
+}
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    	if(err) throw err;
+    	callback(null, isMatch);
+	});
+}
