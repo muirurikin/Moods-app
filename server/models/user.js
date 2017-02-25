c0a9d8a7d37d15528478ea9c76b9b4c3831b189e
Harrisonkamau <kamauharrison87@gmail.com>
2017-02-25 22:54:47 +0300

    use bcrypt to hash new user's password

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,8 @@
-// define the user schema
+// required modules
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+// define the user schema
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
@@ -11,6 +14,19 @@ var UserSchema = new Schema({
 });
 // create a User model using the define Schema
 var User = mongoose.model('User', UserSchema);
+
+// use bcrypt to hash a new user
+module.exports.createUser = function(newUser, callback){
+    bcrypt.genSalt(10, function(err, salt){
+        bcrypt.hash(newUser.password, salt, function(err, hash){
+            // set the user's password equal to the hashed one
+            newUser.password = hash;
+
+            // save the password into db
+            newUser.save(callback);
+        });
+    });
+}
 
 // export the User
 module.exports = User;
