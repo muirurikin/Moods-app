b894ba5eca6d87cb7219af76ad5f970aa9cee927
Zachariah Ngonyani <zachariahn@gmail.com>
2017-02-26 03:41:43 +0300

    All routes should be defined in one single file with will be imported into the main app file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
-  
-    // GET home page
-    router.get('/', ensureAuthenticated, function (req, res) {
-      res.render('index.html');
-         });
+var express        =  require('express');
+var router         =  express.Router();
+var usersRoutes    =  require('./users');
+var sessionsRoutes =  require('./session');
 
-// ensure the user is authenticated 
+// GET home page
+router.get('/', ensureAuthenticated, function (req, res) {
+  res.render('index.html');
+});
+
+router.get('/login',   sessionsRoutes.new);
+router.post('/login',  sessionsRoutes.create);
+router.get('/logout', sessionsRoutes.destroy);
+
+router.get('/users/register',  usersRoutes.new);
+router.post('/users/register', usersRoutes.create);
+
+// ensure the user is authenticated
 function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		// req.flash('error_msg','You are not logged in');
-		res.redirect('/users/login');
-	}
+  if(req.isAuthenticated()){
+    return next();
+  } else {
+    // req.flash('error_msg','You are not logged in');
+    res.redirect('/login');
+  }
 }
 
 module.exports = router;
